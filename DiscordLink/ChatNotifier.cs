9e76e907cea9299a2a478075e44040fa54494802
Spoffy <spoffeh@gmail.com>
2018-02-27 22:49:28 +0000

    [Added] ChatNotifier that polls the ChatServer for new messages and performs a callback when new messages are found.

---
+++ DiscordLink/ChatNotifier.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using Eco.Core.Utils;
+using Eco.Gameplay.Systems.Chat;
+using Eco.Shared.Services;
+using Eco.Shared.Utils;
+using Eco.Simulation.Time;
+
+namespace Eco.Plugins.DiscordLink
+{
+    public class ChatNotifier
+    {
+        public ThreadSafeAction<ChatMessage> OnMessageReceived { get; set; } = new ThreadSafeAction<ChatMessage>();
+
+        private double lastCheckTime = Double.MaxValue;
+        private const int POLL_DELAY = 500;
+
+        public void Initialize()
+        {
+            lastCheckTime = WorldTime.Seconds;
+        }
+        
+        public void Run()
+        {
+            while (true)
+            {
+                var newMessages = ChatServer.GetPlayerMessages(lastCheckTime);
+                newMessages.ForEach(message =>
+                {
+                    Log.Write("Message found, invoking callback.");
+                    OnMessageReceived.Invoke(message);
+                });
+                lastCheckTime = WorldTime.Seconds;
+                Thread.Sleep(POLL_DELAY);
+            }
+        }
+    }
+}
