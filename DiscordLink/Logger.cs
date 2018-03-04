7c4ac385b2208bf3ad3aa3135e73f66f00b49d27
Spoffy <spoffeh@gmail.com>
2018-03-04 10:55:23 +0000

    Added newlines to logging functions.

--- DiscordLink/Logger.cs
+++ DiscordLink/Logger.cs
@@ -6,24 +6,24 @@ namespace Eco.Plugins.DiscordLink
     {
         public static void Debug(string message)
         {
-            Log.Write("DISCORDLINK DEBUG:" + message);
+            Log.Write("DISCORDLINK DEBUG:" + message + "\n");
         }
 
         public static void DebugVerbose(string message)
         {
             #if DEBUGVERBOSE
-            Log.Write("DISCORDLINK DEBUG:" + message);
+            Log.Write("DISCORDLINK DEBUG:" + message + "\n");
             #endif
         }
 
         public static void Info(string message)
         {
-            Log.Write("DISCORDLINK: " + message);
+            Log.Write("DISCORDLINK: " + message + "\n");
         }
 
         public static void Error(string message)
         {
-            Log.Write("DISCORDLINK ERROR:" + message);
+            Log.Write("DISCORDLINK ERROR:" + message + "\n");
         }
     }
 }
