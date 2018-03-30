b8967cdaae55009a7e0b57cf23e270a1be4b30eb
Spoffy <spoffeh@gmail.com>
2018-03-31 00:11:33 +0100

    Fixes ?ecostatus failing when the Server Logo was not a valid URL. Improves debugging when exceptions are thrown in Discord Commands.

--- DiscordLink/DiscordDiscordCommands.cs
+++ DiscordLink/DiscordDiscordCommands.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.Linq;
-using System.Net;
 using System.Threading.Tasks;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
 using Eco.Gameplay.Players;
 using Eco.Plugins.Networking;
-using Eco.Shared.Services;
 
 namespace Eco.Plugins.DiscordLink
 {
@@ -23,6 +21,14 @@ namespace Eco.Plugins.DiscordLink
             return strings.FirstOrDefault(str => !String.IsNullOrEmpty(str)) ?? "";
         }
 
+        public delegate Task DiscordCommand(CommandContext ctx);
+        
+        private static void LogCommandException(Exception e)
+        {
+                Logger.Error("Error occurred while attempting to run that command. Error message: " + e);
+                Logger.Error(e.StackTrace);
+        }
+
         [Command("ping")]
         [Description("Checks if the bot is online.")]
         public async Task Ping(CommandContext ctx)
@@ -34,52 +40,70 @@ namespace Eco.Plugins.DiscordLink
         [Description("Retrieves the current status of the Eco Server")]
         public async Task EcoStatus(CommandContext ctx)
         {
-            var plugin = DiscordLink.Obj;
-            if (plugin == null)
+            try
             {
-                await ctx.RespondAsync(
-                    "The plugin was unable to be found on the server. Please report this to the plugin author.");
-                return;
+                var plugin = DiscordLink.Obj;
+                if (plugin == null)
+                {
+                    await ctx.RespondAsync(
+                        "The plugin was unable to be found on the server. Please report this to the plugin author.");
+                    return;
+                }
+
+                var pluginConfig = plugin.DiscordPluginConfig;
+                var serverInfo = NetworkManager.GetServerInfo();
+
+                var name = FirstNonEmptyString(pluginConfig.ServerName, serverInfo.Name);
+                var description = FirstNonEmptyString(pluginConfig.ServerDescription, serverInfo.Description,
+                    "No server description is available.");
+
+                var addr = IPUtil.GetInterNetworkIP().FirstOrDefault();
+                var serverAddress = String.IsNullOrEmpty(pluginConfig.ServerIP)
+                    ? addr == null ? "No Configured Address"
+                    : addr + ":" + serverInfo.WebPort
+                    : pluginConfig.ServerIP;
+
+                var players = $"{serverInfo.OnlinePlayers}/{serverInfo.TotalPlayers}";
+
+                var timeRemainingSpan = new TimeSpan(0, 0, (int) serverInfo.TimeLeft);
+                var timeRemaining =
+                    $"{timeRemainingSpan.Days} Days, {timeRemainingSpan.Hours} hours, {timeRemainingSpan.Minutes} minutes";
+
+                var timeSinceStartSpan = new TimeSpan(0, 0, (int) serverInfo.TimeSinceStart);
+                var timeSinceStart =
+                    $"{timeSinceStartSpan.Days} Days, {timeSinceStartSpan.Hours} hours, {timeSinceStartSpan.Minutes} minutes";
+
+                var leader = String.IsNullOrEmpty(serverInfo.Leader) ? "No leader" : serverInfo.Leader;
+
+                var builder = new DiscordEmbedBuilder()
+                    .WithColor(EmbedColor)
+                    .WithTitle($"**{name} Server Status**")
+                    .WithDescription(description)
+                    .AddField("Online Players", players)
+                    .AddField("Address", serverAddress)
+                    .AddField("Time Left until Meteor", timeRemaining)
+                    .AddField("Time Since Game Start", timeSinceStart)
+                    .AddField("Current Leader", leader);
+
+                //Add the Server Logo Thumbnail
+                try
+                {
+                    builder = String.IsNullOrWhiteSpace(pluginConfig.ServerLogo)
+                        ? builder
+                        : builder.WithThumbnailUrl(pluginConfig.ServerLogo);
+                }
+                catch (UriFormatException e)
+                {
+                    Logger.Info("Warning: The Configured Server Logo is not a valid URL.");
+                }
+                
+
+                await ctx.RespondAsync("Current status of the server:", false, builder.Build());
             }
-
-            var pluginConfig = plugin.DiscordPluginConfig;       
-            var serverInfo = NetworkManager.GetServerInfo();
-
-            var name = FirstNonEmptyString(pluginConfig.ServerName, serverInfo.Name);
-            var description = FirstNonEmptyString(pluginConfig.ServerDescription, serverInfo.Description,
-                "No server description is available.");
-
-            var addr = IPUtil.GetInterNetworkIP().FirstOrDefault();
-            var serverAddress = String.IsNullOrEmpty(pluginConfig.ServerIP)
-                ? addr == null ? "No Configured Address"
-                : addr + ":" + serverInfo.WebPort
-                : pluginConfig.ServerIP;
-            
-            var players = $"{serverInfo.OnlinePlayers}/{serverInfo.TotalPlayers}";
-            
-            var timeRemainingSpan = new TimeSpan(0, 0, (int) serverInfo.TimeLeft);
-            var timeRemaining = $"{timeRemainingSpan.Days} Days, {timeRemainingSpan.Hours} hours, {timeRemainingSpan.Minutes} minutes";
-            
-            var timeSinceStartSpan = new TimeSpan(0, 0, (int) serverInfo.TimeSinceStart);
-            var timeSinceStart = $"{timeSinceStartSpan.Days} Days, {timeSinceStartSpan.Hours} hours, {timeSinceStartSpan.Minutes} minutes";
-            
-            var leader = String.IsNullOrEmpty(serverInfo.Leader) ? "No leader" : serverInfo.Leader;
-
-            var builder = new DiscordEmbedBuilder()
-                .WithColor(EmbedColor)
-                .WithTitle($"**{name} Server Status**")
-                .WithDescription(description)
-                .AddField("Online Players", players)
-                .AddField("Address", serverAddress)
-                .AddField("Time Left until Meteor", timeRemaining)
-                .AddField("Time Since Game Start", timeSinceStart)
-                .AddField("Current Leader", leader);
-
-            builder = String.IsNullOrWhiteSpace(pluginConfig.ServerLogo)
-                ? builder
-                : builder.WithThumbnailUrl(pluginConfig.ServerLogo);
-
-            await ctx.RespondAsync("Current status of the server:", false, builder.Build());
+            catch (Exception e)
+            {
+                LogCommandException(e);
+            }
         }
 
         
@@ -87,24 +111,32 @@ namespace Eco.Plugins.DiscordLink
         [Description("Lists the players currently online on the server")]
         public async Task PlayerList(CommandContext ctx)
         {
-            var plugin = DiscordLink.Obj;
-            if (plugin == null)
+            try
             {
-                await ctx.RespondAsync(
-                    "The plugin was unable to be found on the server. Please report this to the plugin author.");
-                return;
+                var plugin = DiscordLink.Obj;
+                if (plugin == null)
+                {
+                    await ctx.RespondAsync(
+                        "The plugin was unable to be found on the server. Please report this to the plugin author.");
+                    return;
+                }
+
+                var onlineUsers = UserManager.OnlineUsers.Where(user => user.Client.Connected)
+                    .Select(user => user.Name);
+                var numberOnline = onlineUsers.Count();
+                var message = $"{numberOnline} Online Players:\n";
+                var playerList = String.Join("\n", onlineUsers);
+                var embed = new DiscordEmbedBuilder()
+                    .WithColor(EmbedColor)
+                    .WithTitle(message)
+                    .WithDescription(playerList);
+
+                await ctx.RespondAsync("Displaying Online Players", false, embed);
             }
-
-            var onlineUsers = UserManager.OnlineUsers.Where(user => user.Client.Connected).Select(user => user.Name);
-            var numberOnline = onlineUsers.Count();
-            var message = $"{numberOnline} Online Players:\n";
-            var playerList = String.Join("\n", onlineUsers);
-            var embed = new DiscordEmbedBuilder()
-                .WithColor(EmbedColor)
-                .WithTitle(message)
-                .WithDescription(playerList);
-
-            await ctx.RespondAsync("Displaying Online Players", false, embed);
+            catch (Exception e)
+            {
+                LogCommandException(e);
+            }
         }
     }
 }
