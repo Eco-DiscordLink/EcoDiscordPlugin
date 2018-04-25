14eec6efd9aadc71040b6e4317c5d4a92ff03620
Spoffy <spoffeh@gmail.com>
2018-04-25 23:08:16 +0100

    Adds PagedEnumerable, that adds paging to an IEnumerable<T>

---
+++ DiscordLink/Utilities/PagedEnumerable.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Eco.Plugins.DiscordLink
+{
+    /*
+     * Adds Paging to an IEnumerable.
+     * Recommend NOT using ForEach, as it will only iterate the first page.
+     * Behaviour is undefined with pageSize less than 1.
+     */
+    public class PagedEnumerable<T> : IEnumerable<T>
+    {
+        private readonly IEnumerable<T> _wrappedEnumerable;
+        private readonly int _pageSize;
+        private readonly Func<T, int> _itemToSize;
+
+        public PagedEnumerable(IEnumerable<T> toWrap, int pageSize, Func<T, int> itemToSize)
+        {
+            _wrappedEnumerable = toWrap;
+            _pageSize = pageSize;
+            _itemToSize = itemToSize;
+        }
+
+        public PagedEnumerator<T> GetPagedEnumerator()
+        {
+            return new PagedEnumerator<T>(_wrappedEnumerable.GetEnumerator(), _pageSize, _itemToSize);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return GetPagedEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+    
+    public class PagedEnumerator<T> : IEnumerator<T>
+    {
+        private IEnumerator<T> _wrappedEnumerator;
+        private int _pageSize;
+        private Func<T, int> _itemToSize;
+
+        private int _count = 0;
+        private T _previous;
+
+        public PagedEnumerator(IEnumerator<T> toWrap, int pageSize, Func<T, int> itemToSize)
+        {
+            _wrappedEnumerator = toWrap;
+            _pageSize = pageSize;
+            _itemToSize = itemToSize;
+        }
+
+        public void Dispose()
+        {
+            _wrappedEnumerator.Dispose();
+        }
+
+        //Returns true while there's an item available that fits in the current page.
+        //Returns false when the next item wouldn't fit in the current page.
+        //Starts a new page the next time it's called after "false".
+        public bool MoveNext()
+        {
+            //Don't advance if we've exceeded the page size on the last move - treat the element as the start of a
+            //new page.
+            if (_count <= _pageSize || _pageSize <= 0)
+            {
+                if (!_wrappedEnumerator.MoveNext()) { return false; }
+            }
+            else { _count = 0; }
+
+            _count += _itemToSize(_wrappedEnumerator.Current);
+
+            if (_count > _pageSize)
+            {
+                _previous = _wrappedEnumerator.Current;
+                return false;
+            }
+
+            return true;
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Current => _count > _pageSize ? _previous : _wrappedEnumerator.Current;
+
+        object IEnumerator.Current => Current;
+
+        public void ForEachInPage(Action<T> action)
+        {
+            while (MoveNext()) { action(Current); }
+        }
+    }
+}
