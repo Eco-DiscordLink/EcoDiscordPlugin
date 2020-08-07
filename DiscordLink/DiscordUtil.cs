0e1db168231863eb98b5661120d0336e5460844e
Daniel Bengtsson <daniel.bengtsson@live.com>
2020-08-07 12:31:19 +0200

    Protected SendAsync and ModifyAsync with try catch in case DSharp throws an exception

--- DiscordLink/DiscordUtil.cs
+++ DiscordLink/DiscordUtil.cs
@@ -1,5 +1,6 @@
 ﻿using DSharpPlus;
 using DSharpPlus.Entities;
+using System;
 using System.Threading.Tasks;
 
 namespace Eco.Plugins.DiscordLink
@@ -13,46 +14,62 @@ namespace Eco.Plugins.DiscordLink
 
         public static async Task<DiscordMessage> SendAsync(DiscordChannel channel, string textContent, DiscordEmbed embedContent = null)
         {
-            if (!ChannelHasPermission(channel, Permissions.SendMessages)) { return null; }
+            try
+            {
+                if (!ChannelHasPermission(channel, Permissions.SendMessages)) { return null; }
 
-            if (embedContent == null)
-            {
-                return await channel.SendMessageAsync(textContent, false);
-            }
-            else
-            {
-                // Either make sure we have permission to use embeds or convert the embed to text
-                if (ChannelHasPermission(channel, Permissions.EmbedLinks))
+                if (embedContent == null)
                 {
-                    return await channel.SendMessageAsync(textContent, false, embedContent);
+                    return await channel.SendMessageAsync(textContent, false);
                 }
                 else
                 {
-                    return await channel.SendMessageAsync(MessageBuilder.EmbedToText(textContent, embedContent));
+                    // Either make sure we have permission to use embeds or convert the embed to text
+                    if (ChannelHasPermission(channel, Permissions.EmbedLinks))
+                    {
+                        return await channel.SendMessageAsync(textContent, false, embedContent);
+                    }
+                    else
+                    {
+                        return await channel.SendMessageAsync(MessageBuilder.EmbedToText(textContent, embedContent));
+                    }
                 }
+            }
+            catch(Exception e)
+            {
+                Logger.Error("Error occurred while attempting to send Discord message. Error message: " + e);
+                return null;
             }
         }
 
         public static async Task<DiscordMessage> ModifyAsync(DiscordMessage message, string textContent, DiscordEmbed embedContent = null)
         {
-            if (!ChannelHasPermission(message.Channel, Permissions.ManageMessages)) { return null; }
+            try
+            {
+                if (!ChannelHasPermission(message.Channel, Permissions.ManageMessages)) { return null; }
 
-            if (embedContent == null)
-            {
-                return await message.ModifyAsync(textContent);
-            }
-            else
-            {
-                // Either make sure we have permission to use embeds or convert the embed to text
-                if (ChannelHasPermission(message.Channel, Permissions.EmbedLinks))
+                if (embedContent == null)
                 {
-                    return await message.ModifyAsync(textContent, embedContent);
+                    return await message.ModifyAsync(textContent);
                 }
                 else
                 {
-                    await message.ModifyEmbedSuppressionAsync(true); // Remove existing embeds
-                    return await message.ModifyAsync(MessageBuilder.EmbedToText(textContent, embedContent));
+                    // Either make sure we have permission to use embeds or convert the embed to text
+                    if (ChannelHasPermission(message.Channel, Permissions.EmbedLinks))
+                    {
+                        return await message.ModifyAsync(textContent, embedContent);
+                    }
+                    else
+                    {
+                        await message.ModifyEmbedSuppressionAsync(true); // Remove existing embeds
+                        return await message.ModifyAsync(MessageBuilder.EmbedToText(textContent, embedContent));
+                    }
                 }
+            }
+            catch(Exception e)
+            {
+                Logger.Error("Error occurred while attempting to modify Discord message. Error message: " + e);
+                return null;
             }
         }
     }
