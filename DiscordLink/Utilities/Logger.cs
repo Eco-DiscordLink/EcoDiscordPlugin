072ae2567b1484521b236627d025a06be5c317a2
MonzUn <daniel-bengtsson@live.com>
2020-09-19 13:10:14 +0200

    Added file logging for all non-verbose log messages

--- DiscordLink/Utilities/Logger.cs
+++ DiscordLink/Utilities/Logger.cs
@@ -1,10 +1,16 @@
 using Eco.Shared.Localization;
 using Eco.Shared.Utils;
+using System.IO;
 
 namespace Eco.Plugins.DiscordLink.Utilities
 {
-    public class Logger
+    public static class Logger
     {
+        private static StreamWriter _writer = new StreamWriter(Directory.GetCurrentDirectory() + "\\Mods\\DiscordLink\\Pluginlog.txt", append: true)
+        {
+            AutoFlush = true,
+        };
+
         public static void DebugVerbose(string message)
         {
             if (DLConfig.Data.LogLevel <= LogLevel.DebugVerbose)
@@ -15,33 +21,41 @@ namespace Eco.Plugins.DiscordLink.Utilities
 
         public static void Debug(string message)
         {
+            string fullMessage = "[DiscordLink] DEBUG: " + message + "\n";
+            _writer?.Write(fullMessage);
             if (DLConfig.Data.LogLevel <= LogLevel.Debug)
             {
-                Log.Write(new LocString("[DiscordLink] DEBUG: " + message + "\n"));
+                Log.Write(new LocString(fullMessage));
             }
         }
 
         public static void Warning(string message)
         {
+            string fullMessage = "[DiscordLink] WARNING: " + message + "\n";
+            _writer?.Write(fullMessage);
             if (DLConfig.Data.LogLevel <= LogLevel.Warning)
             {
-                Log.Write(new LocString("[DiscordLink] WARNING: " + message + "\n"));
+                Log.Write(new LocString(fullMessage));   
             }
         }
 
         public static void Info(string message)
         {
+            string fullMessage = "[DiscordLink] " + message + "\n";
+            _writer?.Write(fullMessage);
             if (DLConfig.Data.LogLevel <= LogLevel.Information)
             {
-                Log.Write(new LocString("[DiscordLink] " + message + "\n"));
+                Log.Write(new LocString(fullMessage));   
             }
         }
 
         public static void Error(string message)
         {
+            string fullMessage = "[DiscordLink] ERROR: " + message + "\n";
+            _writer?.Write(fullMessage);
             if (DLConfig.Data.LogLevel <= LogLevel.Error)
             {
-                Log.Write(new LocString("[DiscordLink] ERROR: " + message + "\n"));
+                Log.Write(new LocString(fullMessage));   
             }
         }
     }
