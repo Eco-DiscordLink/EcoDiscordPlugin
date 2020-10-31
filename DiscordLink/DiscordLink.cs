1e2c1a21f891eb9d8a3b657f86878578fa1df233
MonzUn <daniel-bengtsson@live.com>
2020-10-31 19:13:16 +0100

    Eco bot user is now created when the plugin starts, rather than the first time it's needed.

--- DiscordLink/DiscordLink.cs
+++ DiscordLink/DiscordLink.cs
@@ -87,6 +87,8 @@ namespace Eco.Plugins.DiscordLink
                 InitializeIntegrations();
                 UpdateIntegrations(TriggerType.Startup, null);
             };
+
+            _ = EcoUser; // Create the Eco User on startup
         }
 
         public void Shutdown()
