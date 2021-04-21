8d326a6a8033f149bf965309b1783f50135625a2
MonzUn <daniel-bengtsson@live.com>
2021-04-22 00:03:11 +0200

    Added extensions for checking how long a user has been online or offline

--- DiscordLink/Source/Extensions/EcoExtensions.cs
+++ DiscordLink/Source/Extensions/EcoExtensions.cs
@@ -9,6 +9,9 @@ namespace Eco.Plugins.DiscordLink.Extensions
         public static bool IsWhitelisted(this User user) => UserManager.Config.WhiteList.Contains(user.SteamId) || UserManager.Config.WhiteList.Contains(user.SlgId);
         public static bool IsBanned(this User user) => UserManager.Config.BlackList.Contains(user.SteamId) || UserManager.Config.BlackList.Contains(user.SlgId);
         public static bool IsMuted(this User user) => UserManager.Config.MuteList.Contains(user.SteamId) || UserManager.Config.MuteList.Contains(user.SlgId);
+
+        public static double GetSecondsSinceLogin(this User user) => user.IsOnline ? Simulation.Time.WorldTime.Seconds - user.LoginTime : 0.0;
+        public static double GetSecondsSinceLogout(this User user) => user.IsOnline ? 0.0 : Simulation.Time.WorldTime.Seconds - user.LogoutTime;
         #region ChatMessage
 
         public static string FormatForLog(this ChatSent message) => $"Author: {MessageUtils.StripTags(message.Citizen.Name)}\nChannel: {message.Tag}\nMessage: {message.Message}";
