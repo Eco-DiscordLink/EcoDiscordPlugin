de04745390debbcf266cf6784ee91d790d721f29
MonzUn <daniel-bengtsson@live.com>
2021-04-22 00:02:10 +0200

    Added extensions for checking is a user is whitelisted, banned or muted

--- DiscordLink/Source/Extensions/EcoExtensions.cs
+++ DiscordLink/Source/Extensions/EcoExtensions.cs
@@ -1,10 +1,14 @@
 ﻿using Eco.Gameplay.GameActions;
+using Eco.Gameplay.Players;
 using Eco.Plugins.DiscordLink.Utilities;
 
 namespace Eco.Plugins.DiscordLink.Extensions
 {
     public static class EcoExtensions
     {
+        public static bool IsWhitelisted(this User user) => UserManager.Config.WhiteList.Contains(user.SteamId) || UserManager.Config.WhiteList.Contains(user.SlgId);
+        public static bool IsBanned(this User user) => UserManager.Config.BlackList.Contains(user.SteamId) || UserManager.Config.BlackList.Contains(user.SlgId);
+        public static bool IsMuted(this User user) => UserManager.Config.MuteList.Contains(user.SteamId) || UserManager.Config.MuteList.Contains(user.SlgId);
         #region ChatMessage
 
         public static string FormatForLog(this ChatSent message) => $"Author: {MessageUtils.StripTags(message.Citizen.Name)}\nChannel: {message.Tag}\nMessage: {message.Message}";
