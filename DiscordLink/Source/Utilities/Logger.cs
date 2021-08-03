c26e17473fbb5f3f1722965a7b5dea6d28c88962
MonzUn <daniel-bengtsson@live.com>
2021-08-03 20:53:38 +0200

    Updated logger to be compatible with 9.4

--- DiscordLink/Source/Utilities/Logger.cs
+++ DiscordLink/Source/Utilities/Logger.cs
@@ -1,11 +1,11 @@
-using Eco.Core.Utils;
+using Eco.Core.Utils.Logging;
 using static Eco.EM.Framework.Utils.ConsoleColors;
 
 namespace Eco.Plugins.DiscordLink.Utilities
 {
     public static class Logger
     {
-        private static readonly NLogWrapper _pluginLog = NLogWriter.GetConcreteLogger("DiscordLink");
+        private static readonly NLogWriter _pluginLog = NLogManager.GetLogWriter("DiscordLink");
         private static readonly System.ConsoleColor _consoleColor = System.ConsoleColor.Cyan;
         private static readonly string _tag = "[DiscordLink]";
 
@@ -23,37 +23,37 @@ namespace Eco.Plugins.DiscordLink.Utilities
         {
             if (DLConfig.Data.LogLevel <= LogLevel.DebugVerbose)
             {
-                PrintConsoleMultiColored(_tag, _consoleColor, $" DEBUG: {message}", System.ConsoleColor.Gray);
-                _pluginLog.Info($"DEBUG: {message}"); // Verbose debug log messages are only written to the log file if enabled via configuration
+                PrintConsoleMultiColored(_tag, _consoleColor, $" {message}", System.ConsoleColor.Gray);
+                _pluginLog.Write($"DEBUG {message}"); // Verbose debug log messages are only written to the log file if enabled via configuration
             }
         }
 
         public static void Debug(string message)
         {
             if (DLConfig.Data.LogLevel <= LogLevel.Debug)
-                PrintConsoleMultiColored(_tag, _consoleColor, $" DEBUG: {message}", System.ConsoleColor.Gray);
-            _pluginLog.Info($"DEBUG: {message}");
+                PrintConsoleMultiColored(_tag, _consoleColor, $" {message}", System.ConsoleColor.Gray);
+            _pluginLog.Write($"DEBUG: {message}");
         }
 
         public static void Warning(string message)
         {
             if (DLConfig.Data.LogLevel <= LogLevel.Warning)
-                PrintConsoleMultiColored(_tag, _consoleColor, $" WARNING: {message}", System.ConsoleColor.Yellow);
-            _pluginLog.Info($"WARNING: {message}");
+                PrintConsoleMultiColored(_tag, _consoleColor, $" {message}", System.ConsoleColor.Yellow);
+            _pluginLog.WriteWarning(message);
         }
 
         public static void Info(string message)
         {
             if (DLConfig.Data.LogLevel <= LogLevel.Information)
                 PrintConsoleMultiColored(_tag, _consoleColor, $" {message}", System.ConsoleColor.White);
-            _pluginLog.Info($"INFO: {message}");
+            _pluginLog.Write(message);
         }
 
         public static void Error(string message)
         {
             if (DLConfig.Data.LogLevel <= LogLevel.Error)
-                PrintConsoleMultiColored(_tag, _consoleColor, $" ERROR: {message}", System.ConsoleColor.Red);
-            _pluginLog.Info($"ERROR: {message}");
+                PrintConsoleMultiColored(_tag, _consoleColor, $" {message}", System.ConsoleColor.Red);
+            _pluginLog.WriteError(message);
         }
     }
 }
