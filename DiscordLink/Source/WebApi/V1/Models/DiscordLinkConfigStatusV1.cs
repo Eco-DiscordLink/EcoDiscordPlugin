e5a5416f15807dc4be97b8883516f29f92ce47c3
Ethan Snow <bombitmanbomb@gmail.com>
2023-05-06 01:45:44 -0400

    Create DTOs
    Could return JSON, but Typing is used by Swagger

---
+++ DiscordLink/Source/WebApi/V1/Models/DiscordLinkConfigStatusV1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace DiscordLink.Source.WebApi.V1.Models
+{
+    public class ChannelLinkStatusV1
+    {
+        public string Id;
+        public bool Valid;
+    }
+    public class DiscordLinkConfigStatusV1
+    {
+        public bool ClientConnected;
+        public bool ServerId;
+        public bool BotToken;
+        public bool InviteMessage;
+        public IEnumerable<ChannelLinkStatusV1> ChannelLinks;
+    }
+}
---
+++ DiscordLink/Source/WebApi/V1/Models/DiscordLinkPluginStatusV1.cs
@@ -0,0 +1,14 @@
+﻿namespace DiscordLink.Source.WebApi.V1.Models
+{
+    public class DiscordLinkPluginStatusV1
+    {
+        public string DiscordStatus;
+        public string PluginStatus;
+        public string ServerVersion;
+        public string PluginVersion;
+        public int LinkedUsers;
+        public int OptInUsers;
+        public int OptOutUsers;
+        public string ClientName;
+    }
+}
