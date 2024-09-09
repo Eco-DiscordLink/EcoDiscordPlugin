4087814ccdc51c93b7677170038e1338b06a9b39
MonzUn <daniel-bengtsson@live.com>
2024-09-09 22:53:26 +0200

    Removed superflous setting of PersistentData in storage constructor

--- DiscordLink/Source/Systems/Storage.cs
+++ DiscordLink/Source/Systems/Storage.cs
@@ -30,9 +30,7 @@ namespace Eco.Plugins.DiscordLink
 
         // Explicit static constructor to tell C# compiler not to mark type as beforefieldinit
         static DLStorage()
-        {
-            PersistentData = new PersistentStorageData();
-        }
+        { }
 
         private DLStorage()
         { }
